e68f68791a23cd3d3c89e5e30ba041dba873a02f
lenzenc <lenzenc@gmail.com>
2015-06-27 08:17:06 -0500

    Updated travis ci config to run test-all

--- src/it/scala/com/payit/components/mongodb/dao/CrudDAOSpec.scala
+++ src/it/scala/com/payit/components/mongodb/dao/CrudDAOSpec.scala
@@ -1,8 +1,16 @@
 package com.payit.components.mongodb.dao
 
+import com.mongodb.casbah.Imports._
 import org.specs2.mutable.Specification
 
 class CrudDAOSpec extends Specification {
+
+  sequential
+
+  val conn = MongoConnection("localhost", 27017)
+  val db: MongoDB = conn("payit-specs")
+  val modelCollection: MongoCollection = db("spec_models")
+  modelCollection.createIndex(MongoDBObject("name" -> 1), MongoDBObject("unique" -> true))
 
   "testing" >> {
     success
