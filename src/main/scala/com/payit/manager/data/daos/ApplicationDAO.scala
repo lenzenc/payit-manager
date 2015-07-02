e411037ca5aaa80bebb94f8901c8a3eaeaffb950
Craig Lenzen <craigl@15mbp.local>
2015-07-02 13:37:54 -0500

    Added ApplicationDAO with findByApiKey method

--- src/main/scala/com/payit/manager/data/daos/ApplicationDAO.scala
+++ src/main/scala/com/payit/manager/data/daos/ApplicationDAO.scala
@@ -1,8 +1,17 @@
 package com.payit.manager.data.daos
 
-/**
- * Created by craigl on 7/2/15.
- */
-class ApplicationDAO {
+import com.mongodb.casbah.Imports._
+import com.mongodb.casbah.{MongoCollection, MongoDB}
+import com.payit.components.mongodb.dao.CrudDAO
+import com.payit.manager.data.MongoCollections
+import com.payit.manager.data.mappers.ApplicationMapper
+import com.payit.manager.models.Application
+
+class ApplicationDAO(implicit val db: MongoDB) extends CrudDAO[Application] {
+
+  protected lazy val collection: MongoCollection = db(MongoCollections.Applications.toString)
+  protected val mapper = new ApplicationMapper
+
+  def findByApiKey(apiKey: String): Option[Application] = find(MongoDBObject(ApplicationMapper.ApiKey -> apiKey))
 
 }
